c028c5a017d7570a46196f01effdb8f3a5f681eb
Arthur Laterza <arthurvbl05@gmail.com>
2025-11-03 10:10:08 -0300

    Adição dos testes unitários na rota '/sobre' e outros casos da rota '/noticias/busca'

--- BackEnd/tests/test_main.py
+++ BackEnd/tests/test_main.py
@@ -8,24 +8,45 @@ def teste_root():
     """
     Testa o endpoint "/"
     """
-    response = client.get("/")
 
-    # status_code != 200, falhou
-    assert response.status_code == 200
+    response = client.get("/")
+    assert response.status_code == 200  # status_code != 200, falhou
 
     # O conteúdo JSON da resposta deve ser o esperado
     dados_esperados = {
         "message": "SeLiga API - Sistema de Verificação de Notícias",
-        "version": "1.0.0",
+        "version": "0.1.0",
         "endpoints": {
             "docs": "/docs",
             "noticias": "/noticias",
-            "sobre": "/sobre",
-            "equipe": "/equipe"
+            "sobre": "/sobre"
         }
     }
     
     # response != dados_esperados, falhou
+    assert response.json() == dados_esperados
+
+def teste_sobre():
+    """
+    Testa o endpoint "/"
+    """
+
+    # status_code != 200, falhou
+    response = client.get("/sobre")
+    assert response.status_code == 200  # status_code != 200, falhou
+
+    # O conteúdo JSON da resposta deve ser o esperado
+    dados_esperados = {
+        "squad": "Squad 05",
+        "membros": [
+            {"nome": "Gustavo", "papeis": ["Scrum Master", "Back-end"], "imagem_url": "/imagens_equipe/gustavo.jpg"},
+            {"nome": "Arthur", "papeis": ["Product Owner", "Back-end", "DevOps"], "imagem_url": "/imagens_equipe/arthur.jpg"},
+            {"nome": "Marcus", "papeis": ["Front-end", "Banco de Dados"], "imagem_url": "/imagens_equipe/marcus.jpg"},
+            {"nome": "Amanda", "papeis": ["Front-end", "Banco de Dados"], "imagem_url": "/imagens_equipe/amanda.jpg"},
+            {"nome": "Enzo", "papeis": ["Back-end", "Banco de Dados", "Arquitetura"], "imagem_url": "/imagens_equipe/enzo.jpg"},
+            {"nome": "Erick", "papeis": ["DevOps", "Arquitetura"], "imagem_url": "/imagens_equipe/erick.jpg"}
+        ]
+    }
     assert response.json() == dados_esperados
 
 def teste_listar_noticias(monkeypatch):
@@ -87,11 +108,14 @@ fake_id_valido = "60c72b9f9b1d8f001f8e4a9a"
 def teste_buscar_noticia_sucesso(monkeypatch):
     """
     Testa a função de buscar do endpoint /noticias, 
-    considerando que o id é válido  e a notícia foi encontrada
+    considerando que o titulo da notícia foi encontrada
     """
+
+    termo_de_busca = "Politica"     # termo digitado pelo usuário
+
     fake_noticia = {
         "_id": ObjectId(fake_id_valido),
-        "titulo": "Notícia específica",
+        "titulo": "Notícia sobre politica",         # deve conter palavra chave 'politica'
         "texto": "Corpo do texto para a noticia",
         "fonte": "Fonte teste",
         "url": "http://teste.com",
@@ -100,24 +124,57 @@ def teste_buscar_noticia_sucesso(monkeypatch):
         "verificacao": {}
     }
 
-    # substitui o '.find_one' do pymongo
-    def mock_find_one(filtro):
-        assert filtro["_id"] == ObjectId(fake_id_valido)
-        return fake_noticia         # retorna notícia que criamos anteriormente caso não falhe
+    # substitui o '.find' do pymongo
+    # regex localiza o termo específico no título
+    def mock_find(filtro):
+        assert filtro["titulo"]["$regex"] == termo_de_busca
+        assert filtro["titulo"]["$options"] == "i"
+        return [fake_noticia]         # retorna notícia que criamos anteriormente caso não falhe
     
-    monkeypatch.setattr("main.news_collection.find_one", mock_find_one)
+    monkeypatch.setattr("main.news_collection.find", mock_find)
 
-    response = client.get(f"/noticias/{fake_id_valido}")
+    response = client.get(f"/noticias/buscar/{termo_de_busca}")
     assert response.status_code == 200
     data = response.json()
-    
-    assert data["_id"] == fake_id_valido
+
+    assert data["total"] == 1
+    assert len(data["noticias"]) == 1
+    assert data["noticias"][0]["_id"] == fake_id_valido
+    assert data["noticias"][0]["titulo"] == "Notícia sobre politica"
 
 def teste_buscar_noticia_inexistente(monkeypatch):
     """
-    Testa a função de buscar do endpoint /noticias, caso o id da notícia não exista
+    Testa a função de buscar do endpoint /noticias, 
+    mas o titulo da notícia não pode ser encontrado (lista vazia)
     """
 
+    termo_de_busca = "Politica"     # termo digitado pelo usuário    
 
+    def mock_find_vazio(filtro):
+        assert filtro["titulo"]["$regex"] == termo_de_busca
+        return [] # Retorna sem notícias (deveria falhar)
     
+    monkeypatch.setattr("main.news_collection.find", mock_find_vazio)
+
+    response = client.get(f"/noticias/buscar/{termo_de_busca}")
+
+    assert response.status_code == 404      # not found
+    assert response.json()["detail"] == "Nenhuma notícia encontrada com esse título"
+
+def teste_buscar_falha_sistema(monkeypatch):
+    """
+    Testa a função de buscar do endpoint /noticias,
+    mas o banco de dados falha
+    """
+    
+    # um mock que simula um erro por padrão
+    def mock_find_erro(filtro):
+        raise Exception("Erro de regex simulado")
+    
+    monkeypatch.setattr("main.news_collection.find", mock_find_erro)
+
+    response = client.get(f"/noticias/buscar/exemplo")     # qualquer nome deve falhar, independente do termo de busca
+
+    assert response.status_code == 500          # server error
+    assert "Erro ao buscar notícia:" in response.json()["detail"]
 
